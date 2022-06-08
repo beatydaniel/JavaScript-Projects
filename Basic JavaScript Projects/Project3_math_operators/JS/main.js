243eed93cceac2a05d78d5db4896d5a212d782fd
beatydaniel <106612152+beatydaniel@users.noreply.github.com>
2022-06-08 15:16:32 -0700

    Update

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,11 +1,3 @@
-var X = 5;              //declaring the variable    
-X++;                    //+1 to variable
-document.write(X);      //writing the variable after its been increased by 1
-var Y = 5;              
-Y--;                    //-1 to the variable
-document.write ('<br>'+ Y); // seperating the previous document.write and including the Y variable decreased by 1
-
-window.alert(Math.random() * 100 ); //creating a alert that shows a random number 0-100
 
 function addition_function() { // a function that adds the numbers together
     var addition = 5 + 5;
@@ -40,3 +32,20 @@ function negation_operator(){
     var x = 5;
     document.getElementById("Math").innerHTML= "Here 5 changes into a "  + -x; //adding a - sign to a variable can turn the number into a negative.
 }
+
+ function math_random(){
+     Math.random();
+     document.getElementById("Math").innerHTML= Math.random(); //created a random number between 0 and 1
+ }
+
+ function increase_function(){
+     var X=5;
+     X++;
+     document.getElementById("Math").innerHTML="Here a 5 increases by 1 which is " + X++; // using the ++ makes the variable increase by 1
+ }
+
+ function decrease_function(){
+     var Y=5;
+     Y--;
+     document.getElementById("Math").innerHTML="Here a 5 decreases by 1 which is " + Y--; //using the -- makes the variable decrease by 1
+ }
