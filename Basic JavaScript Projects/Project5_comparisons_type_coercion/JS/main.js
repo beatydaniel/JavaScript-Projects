b8dd4d77683b44b15c7557ce416f11ac7e60ebb7
beatydaniel <106612152+beatydaniel@users.noreply.github.com>
2022-06-08 16:50:11 -0700

    Update

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,5 +1,7 @@
 document.write(typeof 6)
 document.write("10"+ 200)
+document.write(10 < 5)
+document.write(10 > 5)
 
 function my_function() {
     document.getElementById("Test").innerHTML= 0/0;
@@ -10,4 +12,12 @@ function my_function2() {
 
 function my_function3(){
     document.getElementById("Test").innerHTML= isNaN("15");
+}
+
+function my_function4(){
+    document.getElementById("Test").innerHTML= (2E310)
+}
+
+function my_function5(){
+    document.getElementById("Test").innerHTML= (-3E310)
 }
