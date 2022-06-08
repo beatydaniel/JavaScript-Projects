30bffd58b80053960c1ec9adf827734311997515
beatydaniel <106612152+beatydaniel@users.noreply.github.com>
2022-06-08 12:46:22 -0700

    Update main.js

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,11 +1,11 @@
-function add(){
+function add(){ //my function to add my Dog text and Cat text
      var Dog = "Shiba ", Cat="Tabby ";
-     document.getElementById("id").innerHTML= Dog + Cat;
+     document.getElementById("id").innerHTML= Dog + Cat;  //setting the id for my html to call to this function
 }
 
 function myfunction1(){
     var sentence = "I am learning JavaScript";
-    sentence += "it is fun!";
+    sentence += "it is fun!"; // a Concatenate that includes that text as well
     document.getElementById("id2").innerHTML =sentence;
 }
 
