88bdfbbc10314f7e8bac8cf3fc7cf6fa3fed0cb5
beatydaniel <106612152+beatydaniel@users.noreply.github.com>
2022-06-08 10:20:59 -0700

    Update

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+var A = "This is a string "; //delcaring a variable and what it displays//
+var B = "This is a different string ";
+var C = "This string uses \"quotation marks\" ";
+var Breed = "Dog ", Pet1 = "Shiba Inu ", Pet2 = "Labrador ", Pet3 ="German Shepard ", Pet4 = "Corgi "; 
+document.write(A);// calling the var A and writing on the page//
+window.alert(B);//callin var B and opens a alert box displaying the text in var B//
+document.write(C);
+document.write("\"This is a concatenating string" + " here is the second part of it\" ");
+document.write(Pet2);
+document.write (5+2)
