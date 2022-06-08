d89290b6bb8fa42703da3c7f2f3b2affc11dbdf5
beatydaniel <106612152+beatydaniel@users.noreply.github.com>
2022-06-08 12:41:38 -0700

    Update

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -3,3 +3,9 @@ function add(){
      document.getElementById("id").innerHTML= Dog + Cat;
 }
 
+function myfunction1(){
+    var sentence = "I am learning JavaScript";
+    sentence += "it is fun!";
+    document.getElementById("id2").innerHTML =sentence;
+}
+
